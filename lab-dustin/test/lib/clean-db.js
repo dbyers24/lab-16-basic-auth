2e9ef029802004729526131de4ae169d452ff31c
dustin <dbyers84@gmail.com>
2017-07-06 13:56:03 -0700

    test

--- lab-dustin/test/lib/clean-db.js
+++ lab-dustin/test/lib/clean-db.js
@@ -1,9 +1,18 @@
 'use strict';
 
 const User = require('../../model/user.js');
+const Recipe = require('../../model/recipe.js');
+
+let removeModelWithHook = (model) => {
+  return model.find({})
+    .then(items => {
+      return Promise.all(items.map(a => a.remove()));
+    });
+};
 
 module.exports = () => {
   return Promise.all([
-    User.remove({}),
+    removeModelWithHook(User),
+    removeModelWithHook(Recipe),
   ]);
 };
--- lab-dustin/test/recipe-router-test.js
+++ lab-dustin/test/recipe-router-test.js
@@ -8,6 +8,7 @@ const expect = require('expect');
 const superagent = require('superagent');
 
 //require app modules
+require('./lib/mock-aws');
 const server = require('../lib/server.js');
 const cleanDB = require('./lib/clean-db.js');
 const mockUser = require('./lib/mock-user');
@@ -23,51 +24,51 @@ describe('testing recipe recipe router', () => {
     it('should respond with a recipe', () => {
       let tempUserData;
       return mockUser.createOne()
-      .then(userData => {
-        tempUserData = userData;
-        return superagent.post(`${API_URL}/api/recipes`)
-        .set('Authorization', `Bearer ${tempUserData.token}`)
-        .field('title', 'Beans')
-        .field('content', 'are delicious')
-        .attach('image', `${__dirname}/assets/hacker.gif`);
-      })
-      .then(res => {
-        expect(res.status).toEqual(200);
-        expect(res.body.title).toEqual('Beans');
-        expect(res.body.content).toEqual('are delicious');
-        expect(res.body.userID).toEqual(tempUserData.user._id.toString());
-        expect(res.body.photoURI).toExist();
-      });
+        .then(userData => {
+          tempUserData = userData;
+          return superagent.post(`${API_URL}/api/recipes`)
+            .set('Authorization', `Bearer ${tempUserData.token}`)
+            .field('title', 'Beans')
+            .field('content', 'are delicious')
+            .attach('image', `${__dirname}/assets/hacker.gif`);
+        })
+        .then(res => {
+          expect(res.status).toEqual(200);
+          expect(res.body.title).toEqual('Beans');
+          expect(res.body.content).toEqual('are delicious');
+          expect(res.body.userID).toEqual(tempUserData.user._id.toString());
+          expect(res.body.photoURI).toExist();
+        });
     });
     it('should respond with a 400', () => {
       let tempUserData;
       return mockUser.createOne()
-      .then(userData => {
-        tempUserData = userData;
-        return superagent.post(`${API_URL}/api/recipes`)
-        .set('Authorization', `Bearer ${tempUserData.token}`)
-        // .field('title', 'Beans')
-        .field('content', 'are delicious')
-        .attach('image', `${__dirname}/assets/hacker.gif`);
-      })
-      .catch(err => {
-        expect(err.status).toEqual(400);
-      });
+        .then(userData => {
+          tempUserData = userData;
+          return superagent.post(`${API_URL}/api/recipes`)
+            .set('Authorization', `Bearer ${tempUserData.token}`)
+          // .field('title', 'Beans')
+            .field('content', 'are delicious')
+            .attach('image', `${__dirname}/assets/hacker.gif`);
+        })
+        .catch(err => {
+          expect(err.status).toEqual(400);
+        });
     });
     it('should respond with a 401', () => {
       let tempUserData;
       return mockUser.createOne()
-      .then(userData => {
-        tempUserData = userData;
-        return superagent.post(`${API_URL}/api/recipes`)
-        // .set('Authorization', `Bearer ${tempUserData.XXXXXXX}`)
-        .field('title', 'Beans')
-        .field('content', 'are delicious')
-        .attach('image', `${__dirname}/assets/hacker.gif`);
-      })
-      .catch(err => {
-        expect(err.status).toEqual(401);
-      });
+        .then(userData => {
+          tempUserData = userData;
+          return superagent.post(`${API_URL}/api/recipes`)
+          // .set('Authorization', `Bearer ${tempUserData.XXXXXXX}`)
+            .field('title', 'Beans')
+            .field('content', 'are delicious')
+            .attach('image', `${__dirname}/assets/hacker.gif`);
+        })
+        .catch(err => {
+          expect(err.status).toEqual(401);
+        });
     });
   });
 });
